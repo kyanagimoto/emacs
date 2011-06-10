65ec611be723cb354d65ceba5269c4e08930df3e
kyanagimoto <koichiyanagimoto@gmail.com>
2011-06-10 15:27:24 +0900

    add elisp.

---
+++ undohist/!Users!koichiyanagimoto!.emacs.d!elisp!flymake-jsl.el
@@ -0,0 +1,2 @@
+
+((digest . "072b1594c9fbf7756dab428987f8af39") (undo-list nil (nil rear-nonsticky nil 1729 . 1730) (nil fontified nil 1 . 1730) (1 . 1730) (t 65535 . 65535)))
---
+++ undohist/!Users!koichiyanagimoto!.emacs.d!elisp!point-undo.el
@@ -0,0 +1,2 @@
+
+((digest . "1e828b473ebbc5b395c3a53dba201a5a") (undo-list nil (1 . 5094) (t 0 . 0)))
